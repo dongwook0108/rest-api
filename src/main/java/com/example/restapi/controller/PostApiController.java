75c76e6b69ff23fb4e350155286acbac7200fa37
dongwook <kdw950108@gmail.com>
2023-04-08 22:31:48 +0900

    Initial commit

---
+++ src/main/java/com/example/restapi/controller/PostApiController.java
@@ -0,0 +1,18 @@
+package com.example.restapi.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+
+    @PostMapping()
+    public void post() {
+
+    }
+
+
+
+}
---
+++ src/main/java/com/example/restapi/model/BookQueryParma.java
@@ -0,0 +1,21 @@
+package com.example.restapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookQueryParma {
+
+    private String category;
+
+    private String issuedYear;
+
+    private String issuedMonth;
+
+    private String issuedDay;
+
+}
