3d734dd1d1256ed0b3e1606cb50d424a32682768
dongwook <kdw950108@gmail.com>
2023-04-18 21:05:01 +0900

    study - memoryDb-CRUD 1. CRUD 2. sorting - comparator

--- src/main/java/com/example/demo/MemoryDbApplication.java
+++ src/main/java/com/example/demo/MemoryDbApplication.java
@@ -1,13 +1,13 @@
-package com.example.restapi;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiApplication {
+public class MemoryDbApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestApiApplication.class, args);
+        SpringApplication.run(MemoryDbApplication.class, args);
     }
 
 }
--- src/test/java/com/example/demo/RestApiApplicationTests.java
+++ src/test/java/com/example/demo/RestApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.restapi;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
